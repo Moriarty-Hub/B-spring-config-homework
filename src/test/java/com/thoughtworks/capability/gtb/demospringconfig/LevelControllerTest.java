a33dcaaf5a586997eecea2f6bf08285fc04bd4d4
Xiangrui Dong <samdongxiangrui@outlook.com>
2020-09-20 14:12:17 +0800

    [xdong] finish the solution 2 of problem 2

---
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest.java
@@ -0,0 +1,27 @@
+package com.thoughtworks.capability.gtb.demospringconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@SpringBootTest
+@TestPropertySource(properties = { "levelNumber=0" })
+@AutoConfigureMockMvc
+public class LevelControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void should_return_basic_when_level_number_is_zero() throws Exception {
+        mockMvc.perform(get("/level"))
+                .andExpect(jsonPath("$", is("basic")));
+    }
+}
